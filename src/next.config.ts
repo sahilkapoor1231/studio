a5835610be44fc1a6f0b296005f471f379a7195b
Sahil Kapoor <sahil.lifelinkr@gmail.com>
2025-06-28 06:45:29 +0000

    The NextJS app server is having trouble starting. Please identify what c

---
+++ src/next.config.ts
@@ -0,0 +1,31 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      // This is needed to fix a compatibility issue between an older version of handlebars (a dependency of Genkit) and webpack.
+      handlebars: 'handlebars/dist/handlebars.js',
+    };
+    return config;
+  },
+};
+
+export default nextConfig;
